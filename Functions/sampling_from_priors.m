aac4d35789df3877c2a50e29f86f8436aa17d956
Matty Hilton <matthewhilton318@hotmail.com>
2025-07-28 22:13:47 +0100

    Optimised sampling_from_priors

--- Functions/sampling_from_priors.m
+++ Functions/sampling_from_priors.m
@@ -11,71 +11,70 @@ arguments (Output)
     samples
 end
 
+% Define paths for saving samples and graphs
+savePath = fullfile(pwd, "Saved_Variables", "Samples.mat");
+graphPath = fullfile(pwd, "Graphs", "Prior_Sampling");
 
-cd("Saved_Variables")
-
-if isfile('Samples.mat') == 1
-    load('Samples.mat', "samples");
-else
-samples = [];
+% Create directories if they do not exist
+if ~isfolder("Saved_Variables")
+    mkdir("Saved_Variables");
+end
+if ~isfolder("Graphs")
+    mkdir("Graphs");
 end
 
-cd ..
+% Load samples if they exist
+if isfile(savePath)
+    load(savePath, "samples");
+else
+    samples = struct(); % Initialize as struct to avoid errors
+end
 
 sessiondata = data(data.NewRunIndex == 1, :);
-
-cd(fullfile("Graphs", "Prior_Sampling"))
 
 set(groot,'defaultFigureVisible','off'); 
 
 for i = 1:numel(options.obsNames)
     for j = 1:numel(options.percNames)
-        filename = "Sample_" + (options.sampling) + (options.percNames{j}) + (options.obsNames{i}) + ".pdf";
+        filename = fullfile(graphPath, "Sample_" + (options.sampling) + (options.percNames{j}) + (options.obsNames{i}) + ".pdf");
         
-        if isfile(filename) == 1
+        if isfile(filename)
             info = pdfinfo(filename);
             numFigures = info.NumPages;
             
             if numFigures >= 100
-            continue    
-
+                continue    
             else
                 delete(filename);
             end
         end
 
         for h = 1:100
-        if options.obsNames{i} == "unitsq_mu3" && (options.percNames{j} == "rw" || options.percnames{j} == "sutton")
-        continue
+            if options.obsNames{i} == "unitsq_mu3" && (options.percNames{j} == "rw" || options.percNames{j} == "sutton")
+                continue
+            end
+            
+            samples.(options.obsNames{i}).(options.percNames{j})(h) = tapas_sampleModel(sessiondata.Correct_Side, options.percArgs{j}, options.obsArgs{i});
+            
+            % Use a switch-case for better readability
+            switch j
+                case 1
+                    tapas_rw_binary_plotTraj(samples.(options.obsNames{i}).(options.percNames{j})(h));
+                case 5
+                    tapas_sutton_k1_binary_plotTraj(samples.(options.obsNames{i}).(options.percNames{j})(h));
+                otherwise
+                    tapas_ehgf_binary_plotTraj(samples.(options.obsNames{i}).(options.percNames{j})(h));
+            end
+            
+            fig = gcf;
+            exportgraphics(fig, filename, 'Append', true);
         end
-        
-        samples.(options.obsNames{i}).(options.percNames{j})(h) = tapas_sampleModel(sessiondata.Correct_Side, options.percArgs{j}, options.obsArgs{i});
-        
-        if j == 1
-            tapas_rw_binary_plotTraj(samples.(options.obsNames{i}).(options.percNames{j})(h));
-        elseif j == 5
-            tapas_sutton_k1_binary_plotTraj(samples.(options.obsNames{i}).(options.percNames{j})(h));
-        else
-            tapas_ehgf_binary_plotTraj(samples.(options.obsNames{i}).(options.percNames{j})(h));
-        end
-        
-        fig = gcf;
-        exportgraphics(fig, filename, 'Append', true);
-        end
-        cd ..
-        cd ..
-
-        cd("Saved_Variables")
-        save("Samples.mat", "samples")
-        
-        cd .. 
-        cd(fullfile("Graphs", "Prior_Sampling"))
-        
     end
 end
 
+% Save samples after processing
+save(savePath, "samples");
+
 set(groot,'defaultFigureVisible','on') 
-cd ..
-cd ..
 
 end
